034f94499068288c8a60a382ae309cac13a0adcc
denisfominykh <2028902d>
2022-01-06 01:50:11 +0200

    feat: add description to home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome</h1>
+      <h1>Welcome!</h1>
+      <p>
+        Remix is a full stack web framework by the creators of React Router.
+        This is a simple blog app to demonstrate the power of Remix.
+      </p>
     </div>
   );
 }

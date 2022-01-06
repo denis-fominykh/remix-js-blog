533c77e87d8e99c1de4000db711ae15ac091e5c0
denisfominykh <2028902d>
2022-01-06 02:33:11 +0200

    feat: add loader to get posts

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,42 @@
+import type { LoaderFunction } from 'remix';
+import { useLoaderData, Link } from 'remix';
+
+interface LoaderData {
+  posts: Array<{ id: number; title: string; body: string }>;
+}
+
+export const loader: LoaderFunction = () => {
+  const data: LoaderData = {
+    posts: [
+      { id: 1, title: 'Post 1', body: 'This is a first test post' },
+      { id: 2, title: 'Post 2', body: 'This is a second test post' },
+      { id: 3, title: 'Post 3', body: 'This is a third test post' },
+    ],
+  };
+
+  return data;
+};
+
 export default function PostItems() {
+  const { posts } = useLoaderData<LoaderData>();
+
   return (
-    <div>
-      <h1>Post Items</h1>
-    </div>
+    <>
+      <div className="page-header">
+        <h1>Posts</h1>
+        <Link className="btn" to="/posts/new">
+          New Post
+        </Link>
+      </div>
+      <ul className="posts-list">
+        {posts.map(({ id, title, body }) => (
+          <li key={id}>
+            <Link to={id.toString()}>
+              <h3>{title}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }

372ef7022144c525332bd27886641d38bd02d9c1
denisfominykh <2028902d>
2022-01-06 02:51:35 +0200

    feat: create a page for a new post with a form for user data

---
+++ app/routes/posts/new.tsx
@@ -0,0 +1,42 @@
+import { Link, redirect } from 'remix';
+import type { ActionFunction } from 'remix';
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const title = form.get('title');
+  const body = form.get('body');
+
+  const fields = { title, body };
+
+  // TODO: submit to database
+
+  return redirect('/posts');
+};
+
+export default function NewPost() {
+  return (
+    <>
+      <div className="page-header">
+        <h1>New Post</h1>
+        <Link to="/posts" className="btn btn-reverse">
+          Back
+        </Link>
+      </div>
+      <div className="page-content">
+        <form method="POST">
+          <div className="form-control">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="body">Post Body</label>
+            <textarea name="body" id="body" />
+          </div>
+          <button className="btn btn-block" type="submit">
+            Add Post
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}

7528d33ec5d335820281c322c01e4e8f12bfcd58
denisfominykh <2028902d>
2022-01-07 02:01:38 +0200

    feat: load single post

--- app/routes/posts/$postId.tsx
+++ app/routes/posts/$postId.tsx
@@ -1,11 +1,32 @@
-import { useParams } from 'remix';
+import { useLoaderData, Link } from 'remix';
+import type { LoaderFunction } from 'remix';
+
+import { db } from '~/utils/db.server';
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const post = await db.post.findUnique({
+    where: { id: params.postId },
+  });
+
+  if (!post) throw new Error('Post not find');
+
+  return { post };
+};
 
 export default function Post() {
-  const params = useParams();
+  const {
+    post: { title, body },
+  } = useLoaderData();
 
   return (
     <div>
-      <h1>Post {params.postId}</h1>
+      <div className="page-header">
+        <h1>{title}</h1>
+        <Link className="btn btn-reverse" to="/posts">
+          Back
+        </Link>
+      </div>
+      <div className="page-content">{body}</div>
     </div>
   );
 }
